3cae4280da4fe94e7cad2265ed5898286bfeaf4a
Amin <amin.esfahani.85@gmail.com>
2021-08-11 23:22:47 -0700

    8.11.2021

--- src/main/java/Test60.java
+++ src/main/java/Test60.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,16 +13,18 @@ public class Test60 {
             for (int i = 0; i < N; i++) {
                 String word = in.next();
                 w[i]=word;
-
+                word.r
             }
             int c=0;
             for (int i=0;i<N;i++){
                 for (int j=i+1;j<N;j++){
                     if(!arr.contains(w[i]) && w[i].equals(w[j])){
                         arr.add(w[i]);
+
                     }
                 }
             }
+        List<List<Integer>>  arra = new ArrayList<>();
             System.out.println(arr.size());
         }
     }
